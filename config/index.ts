7764fa886ad97628c5f0be620b505ccc782db438
zxiaoz123 <155599184+zxiaoz123@users.noreply.github.com>
2024-05-11 09:14:39 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,8 +4,8 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: '医疗健康咨询助手',
+  description: '我可以向您提供准确的医疗咨询服务。有什么能帮到您？',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
